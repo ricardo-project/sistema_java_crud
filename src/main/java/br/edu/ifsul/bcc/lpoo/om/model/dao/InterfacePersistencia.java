bf67d80163fcb49b4f8a601f2af1eb4b44f26f7e
Ricardo Augusto <ricardoamgermani@gmail.com>
2023-12-13 07:37:21 -0300

    Delete src directory

--- src/main/java/br/edu/ifsul/bcc/lpoo/om/model/dao/InterfacePersistencia.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsul.bcc.lpoo.om.model.dao;
-
-public interface InterfacePersistencia {
-    public Boolean conexaoAberta();
-    public void fecharConexao();
-    public Object find(Class c, Object id) throws Exception;
-    public void persist(Object o) throws Exception;
-    public void remover(Object o) throws Exception;
-}
